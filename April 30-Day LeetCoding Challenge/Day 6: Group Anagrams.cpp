1a081dd0c80d1b41fd82329c31fc2ee9673f04cd
Sanskriti Chauhan <skritichauhan@gmail.com>
2020-05-02 10:05:40 +0530

    Create Day 6: Group Anagrams.cpp

---
+++ April 30-Day LeetCoding Challenge/Day 6: Group Anagrams.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) 
+    {
+        if(strs.size()==0)
+            return {{}};
+        unordered_map<string,vector<string>>mp;
+        vector<vector<string>> ans;
+        for(int i=0;i<strs.size();i++)
+        {
+            string temp=strs[i];
+            sort(temp.begin(),temp.end());
+            mp[temp].push_back(strs[i]);       
+        }
+        for(auto it=mp.begin();it!=mp.end();it++)
+        {
+            ans.push_back(it->second);
+        }
+        return ans;
+      
+        
+    }
+};
