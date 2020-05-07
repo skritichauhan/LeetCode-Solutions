3d699c9edbae8a59c6adc836e68810a8e582f732
Sanskriti Chauhan <skritichauhan@gmail.com>
2020-05-07 15:57:03 +0530

    Day 4

---
+++ May 30-Day LeetCoding Challenge/Number Complement.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findComplement(int num) {
+        int result=0;
+        int i=0;
+        while(num)
+        {
+            if((num&1)==0)
+            {
+                result=result+(1<<i);
+            }
+            i++;
+            num=num>>1;
+            
+        }
+        return result;
+    }
+};
