190cdd9cfe0a27c106d372c80e1faf12b6418e77
Sanskriti Chauhan <skritichauhan@gmail.com>
2020-05-18 11:09:32 +0530

    Day 10

---
+++ May 30-Day LeetCoding Challenge/Find the Town Judge.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust)
+    {
+        vector<int>indegree(n+1,0);
+        for(auto &it:trust)
+        {
+            indegree[it[0]]--;
+            indegree[it[1]]++;
+        }
+        for(int i=1;i<=n;i++)
+        {
+            if(indegree[i]==n-1)
+                return i;
+        }
+        return -1;
+    }
+};
