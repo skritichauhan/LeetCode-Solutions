401e8f3e0ce4942432ac059c5865d181b5e01f4b
Sanskriti Chauhan <skritichauhan@gmail.com>
2020-05-21 07:43:56 +0530

    Day 19

---
+++ May 30-Day LeetCoding Challenge/Online Stock Span.cpp
@@ -0,0 +1,19 @@
+class StockSpanner {
+public:
+    
+    vector<pair<int, int>> map;
+    
+    int next(int price) {
+        int span = 1;
+        int index = map.size() - 1;
+        while(index >= 0 && price >= map[index].first){
+            span += map[index].second;
+            index -= map[index].second;
+        }
+        map.push_back(make_pair(price, span));
+        return span;
+    }
+};
+   
+
+ 
